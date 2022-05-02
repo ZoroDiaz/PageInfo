98426c71a3b150810e1c8382fcb61d56cd4a8a0d
Carlos Diaz <diaz123451@gmail.com>
2022-05-02 17:05:26 -0500

    se modifica assets

---
+++ src/assets/assets.js
@@ -0,0 +1,10 @@
+
+import bgFondo from './images/bgFondo.png';
+import ImgProfilev3 from './images/ImgProfilev3.png';
+
+const images = {
+    ImgProfilev3,
+    bgFondo
+}
+
+export default images;
